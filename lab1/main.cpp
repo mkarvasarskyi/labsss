563e0c49055930e4c3747372cfda46bf0f52f63e
mkarvasarskyi <90501195+mkarvasarskyi@users.noreply.github.com>
2021-11-11 21:44:11 +0200

    labs

---
+++ lab1/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  lab1.0
+//
+//  Created by Mischa Karvasarskyi on 13.10.21.
+//
+
+#include <iostream>
+
+int main() {
+    int a;
+    a = sizeof(int);
+    int b;
+    b = sizeof(short);
+    int c;
+    c = sizeof(long);
+    int d;
+    d = sizeof(char);
+    int e;
+    e = sizeof(float);
+    int f;
+    f = sizeof(double);
+}
